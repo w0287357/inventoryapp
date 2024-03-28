3d0e40e89b33128fd7f1372851b79bcddbb0fb90
joshua <joshua_williams5@hotmail.com>
2024-03-28 11:01:42 -0300

    commit

--- iteminventory.sql
+++ iteminventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Mar 27, 2024 at 09:47 PM
+-- Generation Time: Mar 27, 2024 at 11:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `iteminventory`
 --
-CREATE DATABASE IF NOT EXISTS `iteminventory` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `iteminventory`;
 
 -- --------------------------------------------------------
 
@@ -33,6 +31,21 @@ CREATE TABLE `categories` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Fruits'),
+(2, 'Vegetables'),
+(3, 'Dairy'),
+(4, 'Meat'),
+(5, 'Grains'),
+(6, 'Snacks'),
+(7, 'Beverages'),
+(8, 'Frozen'),
+(9, 'Other');
 
 -- --------------------------------------------------------
 
@@ -49,6 +62,18 @@ CREATE TABLE `items` (
   `quantity` int(11) NOT NULL,
   `sku` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `category_id`, `title`, `description`, `price`, `quantity`, `sku`) VALUES
+(2, 1, 'Apple', 'Fresh Apples from the Valley of Nova Scotia!', 1.89, 122, 'FRU001'),
+(3, 2, 'Carrot', 'Fresh Picked Carrots!', 1.39, 19, 'VEG010'),
+(4, 3, 'Eggs', '12 Fresh Eggs from Nova Fresh', 6.29, 56, 'DAI005'),
+(5, 5, 'Bread', 'Uncle Bens Whole Grian', 5.49, 44, 'GRA011'),
+(6, 7, 'Water 24PK', 'Spring Water 24 Pack', 9.99, 9, 'BEV002'),
+(7, 8, 'TV Dinner', 'Cheap, quick and super easy dinner!', 2.89, 17, 'FRO189');
 
 --
 -- Indexes for dumped tables
@@ -75,13 +100,13 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
